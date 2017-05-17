e503719c711d5c03a35b1bfab852ae20bf19f1e4
a-anam <aanam@ca.ibm.com>
2017-05-17 15:22:22 -0400

    Update run_full.sh

--- SMB-2/perf_harness_2/benchmark_prep/uc1/run_full.sh
+++ SMB-2/perf_harness_2/benchmark_prep/uc1/run_full.sh
@@ -2,10 +2,10 @@ source /perf_test/perf_harness_2/stac_benchmark/log.sh
 
 log "** Running use case 1 benchmark"
 
-CMD="./sync_interactive_multi_user.sh 20 120 30 1 5 hdfs://red21:48020/user/root/tpcds_sfactor_12 > /perf_test/perf_data_2/env_output.log 2>&1"
+CMD="./sync_interactive_multi_user.sh 20 120 30 1 5 hdfs://<YOUR_HDFS_HOST>:48020/user/root/tpcds_sfactor_12 > /perf_test/perf_data_2/env_output.log 2>&1"
 cd /perf_test/perf_harness_2
 
 log "$CMD"
-./sync_interactive_multi_user.sh 20 120 30 1 5 hdfs://red21:48020/user/root/tpcds_sfactor_12 > /perf_test/perf_data_2/env_output.log 2>&1
+./sync_interactive_multi_user.sh 20 120 30 1 5 hdfs://<YOUR_HDFS_HOST>:48020/user/root/tpcds_sfactor_12 > /perf_test/perf_data_2/env_output.log 2>&1
 
 log "** Use case 1 complete"

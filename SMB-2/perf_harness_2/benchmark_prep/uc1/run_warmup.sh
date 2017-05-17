b3401f38c6bee4c7ebabf7c22e723b5b297edb7a
a-anam <aanam@ca.ibm.com>
2017-05-17 15:22:37 -0400

    Update run_warmup.sh

--- SMB-2/perf_harness_2/benchmark_prep/uc1/run_warmup.sh
+++ SMB-2/perf_harness_2/benchmark_prep/uc1/run_warmup.sh
@@ -1,10 +1,10 @@
 source /perf_test/perf_harness_2/stac_benchmark/log.sh
-CMD="sync_interactive_multi_user.sh 3 120 5 1 5 hdfs://red21:48020/user/root/tpcds_sfactor_12 > /perf_test/perf_data_2/env_output.log 2>&1"
+CMD="sync_interactive_multi_user.sh 3 120 5 1 5 hdfs://<YOUR_HDFS_HOST>:48020/user/root/tpcds_sfactor_12 > /perf_test/perf_data_2/env_output.log 2>&1"
 log "** Running warmup"
 
 cd /perf_test/perf_harness_2
 
 log "$CMD"
-./sync_interactive_multi_user.sh 3 120 5 1 5 hdfs://red21:48020/user/root/tpcds_sfactor_12 > /perf_test/perf_data_2/env_output.log 2>&1
+./sync_interactive_multi_user.sh 3 120 5 1 5 hdfs://<YOUR_HDFS_HOST>:48020/user/root/tpcds_sfactor_12 > /perf_test/perf_data_2/env_output.log 2>&1
 
 log "** Warmup complete"
